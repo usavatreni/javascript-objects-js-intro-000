a078f43cb375c83bae1c562d591b369abc9a996c
Petar Cale <usavatreni@gmail.com>
2020-11-16 03:55:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Tupac: "All Eyez On Me"};
 
 function updatePlaylist(playlist, artistName, songTitle ) {
-  playlist.artistname = songTitle
+  playlist.artistName = songTitle
   return playlist;
 }
